954a61228d69c972e3c1dec3bfcc29325c985a2f
ignaciocaceres-star <ignacio.caceres@mi.unc.edu.ar>
2026-08-29 17:35:06 -0300

    ignacio-configuracion-de-pines

---
+++ configuracion de pines/build/default/debug/TP2.o.d
@@ -0,0 +1,3 @@
+build/default/debug/TP2.o:  \
+	B:/trabajos\ mplab/nuebo_poryecto.X/TP2.asm  \
+
---
+++ configuracion de pines/build/default/production/TP2.o.d
@@ -0,0 +1,3 @@
+build/default/production/TP2.o:  \
+	B:/trabajos\ mplab/nuebo_poryecto.X/TP2.asm  \
+
